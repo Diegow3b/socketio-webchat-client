941918d3801468e386ad43ac0f7efdc6b2691c64
Diegow3bd <diegowebd@hotmail.com>
2016-09-26 00:03:22 -0300

    updates

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -11,25 +11,31 @@ export class HomePage {
     socket: any;
     chat_input: string;
     chats: any;
-    zone: any;
     user: string;
+    data: any;
+    id: number;
 
     constructor(public navCtrl: NavController, params: NavParams) {
 
         this.user = params.get("user");
+        this.id = new Date().getMilliseconds();
         this.chats = [];
         this.chat_input ='';
-        // this.socket = io.connect('http://localhost:3000');
-        this.socket = io.connect('http://iosocketserver-diegolayout.rhcloud.com:8000');
-        this.socket.on('message', (msg) => {
-            this.chats.push(msg);
+        this.socket = io.connect('http://localhost:3000');
+        //this.socket = io.connect('http://iosocketserver-diegolayout.rhcloud.com:8000');
+        this.socket.on('data', (data) => {
+            this.chats.push(data);
         });
     }
 
     send(msg) {
         if(msg != ''){
-            this.socket.emit('message', msg);
-            this.socket.emit('user', this.user);
+            this.data = {
+                id: this.id,
+                user: this.user,
+                message: msg
+            }
+            this.socket.emit('data', this.data);
         }
         this.chat_input = '';
     }
